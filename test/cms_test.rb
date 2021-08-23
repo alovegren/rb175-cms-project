0a9582eb85c5e74054daa876a0fd336f7568c32a
Missy Lovegren <audrey.lovegren@gmail.com>
2021-08-23 13:32:02 -0400

    Test index and file viewing pages are displaying correctly

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -15,9 +15,23 @@ class CMSTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    @current_dir = File.expand_path("..", __FILE__)
+    @files = Dir.glob("#{@current_dir}/data/*").map { |file| File.basename(file) }
+  end
+
   def test_index
     get "/"
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert @files.all? { |file| last_response.body.include?(file) }
+  end
+
+  def test_view_file
+    get "/history.txt"
+    filename = "#{@current_dir}/../data/history.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_equal File.read(filename), last_response.body
   end
 end
