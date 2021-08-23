397d75bfd4de2dbe2d720315caa6aea7d9370cb7
Missy Lovegren <audrey.lovegren@gmail.com>
2021-08-23 11:22:35 -0400

    Update homepage with list of files in system

--- cms.rb
+++ cms.rb
@@ -4,6 +4,6 @@ require "sinatra/content_for"
 require "tilt/erubis"
 
 get '/' do
-  @files = Dir.glob("/public/*").map { |file| File.basename(file) }
+  @files = Dir.glob("./public/*").map { |file| File.basename(file) }
   erb :home
 end
